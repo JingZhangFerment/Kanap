961619556ffabdb01cd88f9c2691cdf99c377fbf
Jing ZHANG <jing.zhang.ferment@gmail.com>
2022-02-07 11:51:30 +0100

    décomposer la fonction displayCart en plusieurs fonctions

--- front/js/cart.js
+++ front/js/cart.js
@@ -24,104 +24,125 @@ async function displayCart(getCartData) {
 
     const productDataFromApi = await getProduct(cartData.id);
 
-    createDivArticle(cartData);
+    const cartItem = createDivArticle(cartData.id, cartData.color);
 
-    //insertion de la div "cart__item__img"
-    const cartItemDivImage = document.createElement("div");
-    cartItem.appendChild(cartItemDivImage);
-    cartItemDivImage.classList.add("cart__item__img");
+    const cartItemDivImage = createDivCartItemImg(cartItem);
 
-    //insertion de la balise "image"
-    const cartItemImage = document.createElement("img");
-    cartItemDivImage.appendChild(cartItemImage);
-    cartItemImage.src = productDataFromApi.imageUrl;
-    cartItemImage.alt = productDataFromApi.altTxt;
-
-    //insertion de la div "cart__item__content"
-    const cartItemContent = document.createElement("div");
-    cartItem.appendChild(cartItemContent);
-    cartItemContent.classList.add("cart__item__content");
-
-    //insertion de la div "cart__item__content__description"
-    const cartItemContentDescription = document.createElement("div");
-    cartItemContent.appendChild(cartItemContentDescription);
-    cartItemContentDescription.classList.add(
-      "cart__item__content__description"
+    const cartItemImage = createCartItemImg(
+      cartItemDivImage,
+      productDataFromApi
     );
 
-    //insertion de <h2>
-    const cartItemName = document.createElement("h2");
-    cartItemContentDescription.appendChild(cartItemName);
-    cartItemName.textContent = productDataFromApi.name;
+    const cartItemContent = createCartItemContent(cartItem);
 
-    //insertion de <p> pour la couleur
-    const cartItemColor = document.createElement("p");
-    cartItemContentDescription.appendChild(cartItemColor);
-    cartItemColor.textContent = cartData.color;
+    const cartItemContentDescription =
+      createCartItemContentDescription(cartItemContent);
 
-    //insertion de <p> pour le prix
+    const cartItemName = createDivH2(
+      cartItemContentDescription,
+      productDataFromApi
+    );
+
+    const cartItemColor = createDivColor(cartItemContentDescription, cartData);
+
     createItemPrice(cartItemContentDescription, productDataFromApi.price);
 
-    //insertion de la div "cart__item__content__settings"
-    const cartItemContentSettings = document.createElement("div");
-    cartItemContent.appendChild(cartItemContentSettings);
-    cartItemContentSettings.classList.add("cart__item__content__settings");
+    const cartItemContentSettings =
+      createDivCartItemContentSettings(cartItemContent);
 
-    //insertion de la div "cart__item__content__settings__quantity"
-    const cartItemContentSettingsQuantity = document.createElement("div");
-    cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
-    cartItemContentSettingsQuantity.classList.add(
-      "cart__item__content__settings__quantity"
+    const cartItemContentSettingsQuantity = createDivCartItemSettingsQuantity(
+      cartItemContentSettings
     );
 
-    //insertion de "Qté :"
-    const cartItemDivQuantity = document.createElement("p");
-    cartItemContentSettingsQuantity.appendChild(cartItemDivQuantity);
-    cartItemDivQuantity.textContent = "Qté :";
+    const cartItemDivQuantity = createCartItemDivQuantity(
+      cartItemContentSettingsQuantity
+    );
 
-    //insertion de <input> pour la quantité
     const cartItemQuantity = createQuantityInput(
       cartItemContentSettingsQuantity,
       cartData.quantity
     );
 
-    //insertion de div "cart__item__content__settings__delete"
-    const cartItemContentSettingsDelete = document.createElement("div");
-    cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
-    cartItemContentSettingsDelete.classList.add(
-      "cart__item__content__settings__delete"
-    );
+    const cartItemContentSettingsDelete =
+      createDivCartItemContentSettingsDelete(cartItemContentSettings);
 
-    //insertion de <p> "deleteItem"
-    const deleteItem = document.createElement("p");
-    cartItemContentSettingsDelete.appendChild(deleteItem);
-    deleteItem.classList.add("deleteItem");
-    deleteItem.textContent = "Supprimer";
+    const deleteItem = createDeleteItem(cartItemContentSettingsDelete);
 
     registerDeleteAction(deleteItem, cartData, getCartData);
 
     changeCartItemQuantity(cartItemQuantity, cartData, getCartData);
 
     myTotal += parseInt(cartItemQuantity.value);
-    console.log(myTotal);
   }
-
-  let totalQuantity = document.getElementById("totalQuantity");
-  totalQuantity.textContent += myTotal;
-  console.log(myTotal);
+  calculateTotals(myTotal);
 }
 
 displayCart(getCartData);
 
 //insertion de la balise "article"
-function createDivArticle(cartData) {
+function createDivArticle(id, color) {
   const cartItem = document.createElement("article");
   document.querySelector("#cart__items").appendChild(cartItem);
   cartItem.classList.add("cart__item");
-  cartItem.setAttribute("data-id", cartData.id);
-  cartItem.setAttribute("data-color", cartData.color);
+  cartItem.setAttribute("data-id", id);
+  cartItem.setAttribute("data-color", color);
 
   return cartItem;
+}
+
+//insertion de la div "cart__item__img"
+function createDivCartItemImg(parent) {
+  const cartItemDivImage = document.createElement("div");
+  parent.appendChild(cartItemDivImage);
+  cartItemDivImage.classList.add("cart__item__img");
+
+  return cartItemDivImage;
+}
+
+//insertion de la balise "image"
+function createCartItemImg(parent, productDataFromApi) {
+  const cartItemImage = document.createElement("img");
+  parent.appendChild(cartItemImage);
+  cartItemImage.src = productDataFromApi.imageUrl;
+  cartItemImage.alt = productDataFromApi.altTxt;
+
+  return cartItemImage;
+}
+
+//insertion de la div "cart__item__content"
+function createCartItemContent(parent) {
+  const cartItemContent = document.createElement("div");
+  parent.appendChild(cartItemContent);
+  cartItemContent.classList.add("cart__item__content");
+
+  return cartItemContent;
+}
+
+//insertion de la div "cart__item__content__description"
+function createCartItemContentDescription(parent) {
+  const cartItemContentDescription = document.createElement("div");
+  parent.appendChild(cartItemContentDescription);
+  cartItemContentDescription.classList.add("cart__item__content__description");
+
+  return cartItemContentDescription;
+}
+
+//insertion de <h2>
+function createDivH2(parent, productDataFromApi) {
+  const cartItemName = document.createElement("h2");
+  parent.appendChild(cartItemName);
+  cartItemName.textContent = productDataFromApi.name;
+
+  return cartItemName;
+}
+
+//insertion de <p> pour la couleur
+function createDivColor(parent, cartData) {
+  const cartItemColor = document.createElement("p");
+  parent.appendChild(cartItemColor);
+  cartItemColor.textContent = cartData.color;
+
+  return cartItemColor;
 }
 
 //insertion de la p - prix
@@ -129,6 +150,37 @@ function createItemPrice(parent, price) {
   const cartItemPrice = document.createElement("p");
   parent.appendChild(cartItemPrice);
   cartItemPrice.textContent = `${price} €`;
+}
+
+//insertion de la div "cart__item__content__settings"
+
+function createDivCartItemContentSettings(parent) {
+  const cartItemContentSettings = document.createElement("div");
+  parent.appendChild(cartItemContentSettings);
+  cartItemContentSettings.classList.add("cart__item__content__settings");
+
+  return cartItemContentSettings;
+}
+
+//insertion de la div "cart__item__content__settings__quantity"
+
+function createDivCartItemSettingsQuantity(parent) {
+  const cartItemContentSettingsQuantity = document.createElement("div");
+  parent.appendChild(cartItemContentSettingsQuantity);
+  cartItemContentSettingsQuantity.classList.add(
+    "cart__item__content__settings__quantity"
+  );
+
+  return cartItemContentSettingsQuantity;
+}
+
+//insertion de "Qté :"
+function createCartItemDivQuantity(parent) {
+  const cartItemDivQuantity = document.createElement("p");
+  parent.appendChild(cartItemDivQuantity);
+  cartItemDivQuantity.textContent = "Qté :";
+
+  return createCartItemDivQuantity;
 }
 
 //insertion d'input - quantité
@@ -143,6 +195,28 @@ function createQuantityInput(parent, quantity) {
   cartItemQuantity.setAttribute("value", quantity);
 
   return cartItemQuantity;
+}
+
+//insertion de div "cart__item__content__settings__delete"
+function createDivCartItemContentSettingsDelete(parent) {
+  const cartItemContentSettingsDelete = document.createElement("div");
+  parent.appendChild(cartItemContentSettingsDelete);
+  cartItemContentSettingsDelete.classList.add(
+    "cart__item__content__settings__delete"
+  );
+
+  return cartItemContentSettingsDelete;
+}
+
+//insertion de <p> "deleteItem"
+
+function createDeleteItem(parent) {
+  const deleteItem = document.createElement("p");
+  parent.appendChild(deleteItem);
+  deleteItem.classList.add("deleteItem");
+  deleteItem.textContent = "Supprimer";
+
+  return deleteItem;
 }
 
 //suppression d'un produit selon son id ET sa couleur
@@ -185,4 +259,14 @@ function changeCartItemQuantity(cartItemQuantity, cartData, getCartData) {
 
     localStorage.setItem("myCart", JSON.stringify(findCartData));
   });
+}
+
+//calculer la quantité totale et le prix total
+
+function calculateTotals(myTotal) {
+  let totalQuantity = document.getElementById("totalQuantity");
+  totalQuantity.textContent += myTotal;
+  console.log(myTotal);
+
+  let totalPrice = document.getElementById("totalPrice");
 }

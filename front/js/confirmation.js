7851234cb8bc6d05271654514dc35dd62d712cf2
Jing ZHANG <jing.zhang.ferment@gmail.com>
2022-02-09 23:19:18 +0100

    afficher le numéro de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,8 +1,8 @@
 //insérer le numéro de commande sur la page de confirmation
 const orderId = document.getElementById("orderId");
+orderId.textContent = getOrderId();
 
-
-//récupérer le "orderID" depuis l'API et ajout sur la navigateur 
+//récupérer le "orderID" depuis l'URL
 function getOrderId() {
     return new URL(location.href).searchParams.get("order");
   }
